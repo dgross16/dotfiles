940b2b21f7b01a67e9c615947d03a368c18d7a20
dgross16 <danielgross13382@gmail.com>
2024-03-25 20:32:34 -0400

    first commit

---
+++ nvim/lua/dani/init.lua
@@ -0,0 +1,4 @@
+require("dani.set")
+require("dani.remap")
+require("dani.lualine")
+require("dani.packer")
---
+++ nvim/lua/dani/packer.lua
@@ -0,0 +1,43 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+      }
+    }
+  use 'RRethy/nvim-base16'
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+  use 'nvim-lua/plenary.nvim'
+  use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'ThePrimeagen/vim-be-good'
+end)
